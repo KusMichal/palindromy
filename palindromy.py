faffbc4b83e641bc33c9d57275d91ef0c2ae4c3d
Michal <kusmichal.ku@gmail.com>
2023-03-26 17:59:32 +0200

    usprawniono

--- palindromy.py
+++
@@ -1,7 +0,0 @@
-def palin(word):
-    if(word==word[::-1]):
-        return "to słowo to palindrom"
-    else:
-        return "to słowo nie jest palindromem"
-print(palin("ala"))
-print(palin("Kodilla"))
